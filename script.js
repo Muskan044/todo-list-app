f50f96f623fc2f8abcfecba8cc034a4af8811199
Muskan044 <muskankhan430430@gmail.com>
2024-09-22 20:13:16 +0500

    create remove button in js

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ function addTask() {
     var li = document.createElement('li');
     li.textContent = taskInput.value;
 
-    // // Create a remove button
-    // var removeButton = document.createElement('button');
-    // removeButton.textContent = 'Remove';
-    // removeButton.className = 'remove-btn';
-    // removeButton.onclick = function () {
-    //     taskList.removeChild(li);
-    // };
+    // Create a remove button
+    var removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.className = 'remove-btn';
+    removeButton.onclick = function () {
+        taskList.removeChild(li);
+    };
 
     // // Add click event to mark as completed
     // li.onclick = function () {
